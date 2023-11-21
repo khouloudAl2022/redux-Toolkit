d8d6fc65efb2832218a3459217acbf24be000b9a
kiki <aloui.khouloud11@gmail.com>
2023-11-21 10:51:25 +0100

    configure Redux store with bookSlice reducer

---
+++ src/store/bookSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getBooks = createAsyncThunk(
+  "book/getBooks",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch(
+        "http://my-json-server.typicode.com/dmitrijt9/book-api-mock/books"
+      );
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+// create slice book
+const bookSlice = createSlice({
+  name: "book",
+  initialState: { books: null },
+  extraReducers: {
+    [getBooks.fulfilled]: (state, action) => {
+      console.log(action);
+    },
+    [getBooks.rejected]: (state, action) => {
+      console.log(action);
+    },
+    [getBooks.pending]: (state, action) => {
+      console.log(action);
+    },
+  },
+});
+export default bookSlice.reducer;
